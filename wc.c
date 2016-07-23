020b50e0bfa131761d5593bb20e4c28e0e1ee21f
Alterith <dbhugwan15@gmail.com>
2016-07-23 15:27:48 +0200

    working just get total and clean up

--- wc.c
+++ wc.c
@@ -20,17 +20,37 @@ int main (int argc, char *argv[]) {
 
 void wc( FILE *infile, char *inname) {
 	//printf("%s: ", inname);
-	//required data values	
+	//required data values	 
 	int lCount = 0;
 	int bCount = 0;
 	int wCount = 0;
 	
 	FILE* f = infile;
-	//!file end of file
-	do{
-		char line[150];
-		fgets(line,150,f);
-		printf("%s",line);
-	}while(!feof(f));
-	//printf("%d %d %d\n",lCount, wCount, bCount);	
+	char sChar;
+	char pChar = ' ';
+	bool wStart = false;
+	while((sChar=fgetc(f))!=EOF)
+	{
+		bCount = bCount+1;
+		if(sChar == '\n'){
+			lCount = lCount+1;		
+		}
+		
+		if (isspace(sChar)){  // isspace() checks for whitespace characters '  ', '\t', '\n','\r, so no need to write like this (isspace(n) || n == '\n')
+   			wStart=false; //outside of word.     
+		}else if(wStart==false){
+    			wCount++;
+    			wStart=true; //inside of word.
+ 		}
+	}
+	printf("%d",lCount);
+	printf("\t%d",wCount);
+	printf("\t%d",bCount);
+	printf("\t%s\n",inname);
+	
 }
+
+
+
+
+
